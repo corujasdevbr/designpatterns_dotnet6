a5b8d8f5fc58b359b8b38fb88e2d9c96aeec9498
Fernando Henrique <fernando.guerra@corujasdev.com.br>
2022-08-11 15:57:24 -0300

    feat: Implementation Singleton

---
+++ src/CorujasDev.DesignPatterns.Singleton/Implementation.cs
@@ -0,0 +1,20 @@
+﻿
+namespace CorujasDev.DesignPatterns.Singleton
+{
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public class Logger
+    {
+        //Validate that the class in your building will only be instanced once
+        private static readonly Lazy<Logger> _lazyLogger
+            = new Lazy<Logger>(() => new Logger());
+
+        public static Logger Instance => _lazyLogger.Value;
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
+    }
+}
---
+++ src/CorujasDev.DesignPatterns.Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CorujasDev.DesignPatterns.Singleton;
+
+Console.WriteLine("Implementation Singleton");
+
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if(instance1 == instance2 && instance2 == Logger.Instance)
+{
+    Console.WriteLine("Instances are the same");
+}
+
+instance1.Log($"Message from {instance1}");
+instance2.Log($"Message from {instance2}");
+Logger.Instance.Log($"Message from {Logger.Instance}");
+
+Console.ReadLine();
